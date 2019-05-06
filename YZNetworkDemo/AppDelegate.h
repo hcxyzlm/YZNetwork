3fc9de05e3581c53b685e63260bb9b72d617f561
zhuo <hczyzlm@126.com>
2019-05-06 17:28:22 +0800

    feat: 初始化

---
+++ YZNetworkDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YZNetwork
+//
+//  Created by zhuo on 2019/5/6.
+//  Copyright © 2019 zhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YZNetworkDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YZNetwork
+//
+//  Created by zhuo on 2019/5/6.
+//  Copyright © 2019 zhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
